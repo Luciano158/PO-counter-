5ab58999fd55837451a5e1338f13a50e9c500e5d
luciano senekal <lucianosenekal@gmail.com>
2022-12-02 22:34:26 -0800

    solo project

---
+++ Calculator/solo/index.js
@@ -0,0 +1,30 @@
+let HOME = document.getElementById("home-el")
+
+let GUEST = document.getElementById("guest-el")
+
+
+let home = 0
+let guest = 0
+
+function addone() {
+
+   home +=  + 1
+   
+   HOME.textContent = home
+}
+
+function addtwo() {
+
+    home +=  + 2
+    
+    HOME.textContent = home
+ }
+
+ function addtree() {
+
+    home +=  + 3
+    
+    HOME.textContent = home
+ }
+
+ 
